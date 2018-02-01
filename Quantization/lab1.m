7fb7f291fe8c7fb156b8b42cdf7b163aa9ea7ad5
Tim Nguyen <timmyn7@gmail.com>
2018-02-01 17:03:08 -0500

    Finished SQNR

--- Quantization/lab1.m
+++ Quantization/lab1.m
@@ -65,49 +65,58 @@ stem(z,'b')
 
 %% 5) Quantization (Rounding)
 
+%%
 % This script creates a signal, and then quantizes it to a specified number
 % of bits.  It then calculates the quantization error.
 % see if you run the script.
 
 fprintf('\nE71 Lab, Sampling and Quantization\n');
 
-b=2;                            % Number of bits.
-N=120;                          % Number of samples in final signal.
-n=0:(N-1);                 %Index
-
 % Choose the input type.
 choice = questdlg('Choose input','Input',...
     'Sine','Sawtooth','Random','Random');
 
-fprintf('Bits = %g, levels = %g, signal = %s.\n', b, 2^b, choice);
+for b=[2,4,6,8,10]
 
-% Create the  input data sequence.
-switch choice
-    case 'Sine'
-        x=sin(2*pi*n/N);
-    case 'Sawtooth'
-        x=sawtooth(2*pi*n/N);
-    case 'Random'
-        x=randn(1,N);       % Random data
-        x=x/max(abs(x));    % Scale to +/- 1
+    N=120;                          % Number of samples in final signal.
+    n=0:(N-1);                 %Index
+
+    fprintf('Bits = %g, levels = %g, signal = %s.\n', b, 2^b, choice);
+
+    % Create the  input data sequence.
+    switch choice
+        case 'Sine'
+            x=sin(2*pi*n/N);
+        case 'Sawtooth'
+            x=sawtooth(2*pi*n/N);
+        case 'Random'
+            x=randn(1,N);       % Random data
+            x=x/max(abs(x));    % Scale to +/- 1
+    end
+
+    % Signal is restricted to between -1 and +1.
+    x(x>=1)=(1-eps);            % Make  signal from -1 to just less than 1.
+    x(x<-1)=-1;
+
+    % Quantize a signal to "b" bits.  
+    xq=floor((x+1)*2^(b-1));    % Signal is one of 2^n int values (0 to 2^n-1)
+    xq=xq/(2^(b-1));            % Signal is from 0 to 2 (quantized)
+    xq=xq-(2^(b)-1)/2^(b);      % Shift signal down (rounding)
+
+    xe=x-xq;                    % Quantization error
+ 
+    % Calculate SQNR
+    SQNR = 10*log10(sum(x.^2)/sum(xe.^2));
+    SQNR2 = 1.76+6.02*b;
+    
+    fprintf('SQNR = %g, SQNR2 = %g\n', SQNR, SQNR2);
+    stem(x,'b');
+    hold on;
+    stem(xq,'r');
+    hold on;
+    stem(xe,'g');
+    legend('exact','quantized','error','Location','Southeast')
+    title(sprintf('Signal, Quantized signal and Error for %g bits, %g quantization levels',b,2^b));
+    hold off
+
 end
-
-% Signal is restricted to between -1 and +1.
-x(x>=1)=(1-eps);            % Make  signal from -1 to just less than 1.
-x(x<-1)=-1;
-
-% Quantize a signal to "b" bits.  
-xq=floor((x+1)*2^(b-1));    % Signal is one of 2^n int values (0 to 2^n-1)
-xq=xq/(2^(b-1));            % Signal is from 0 to 2 (quantized)
-xq=xq-(2^(b)-1)/2^(b);      % Shift signal down (rounding)
-
-xe=x-xq;                    % Quantization error
-
-stem(x,'b');
-hold on;
-stem(xq,'r');
-hold on;
-stem(xe,'g');
-legend('exact','quantized','error','Location','Southeast')
-title(sprintf('Signal, Quantized signal and Error for %g bits, %g quantization levels',b,2^b));
-hold off
