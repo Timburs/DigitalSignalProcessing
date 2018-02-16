36458992b8a8fa464fd9de2c9efcd20437e64b97
Tim Nguyen <timmyn7@gmail.com>
2018-02-15 21:44:11 -0500

    DONE

--- DSP for Genes/lab2a.m
+++ DSP for Genes/lab2a.m
@@ -1,4 +1,5 @@
 %% Open file and read human genome
+clc; clear;
 f1='humhbb.txt'; 
 f=fopen(f1);        
 seq=fread(f,'*char')';
@@ -11,9 +12,12 @@ end
 
 % Find beginning of DNA sequence and read to end-of-line (cr='13')
 x=findstr(seq,'ORIGIN');   seq=seq(x:end);
-x=findstr(seq,eol);        seq=seq(x:end);
+x=findstr(seq,eol);         seq=seq(x:end);
 
 seq=seq(isletter(seq));   % just take letters (drop numbers and spaces).
+if strcmp(f1,'humhbb.txt')
+    seq = seq(62205:63628);
+end
 s1a=double((seq=='a'));   % find all of the letter 'a' and replace with 1.
 s1g=double((seq=='g'));   % find all of the letter 'g' and replace with 1.
 s1t=double((seq=='t'));   % find all of the letter 't' and replace with 1.
@@ -70,27 +74,46 @@ fclose(f);
 
 %% Perform Correlations
 r1a=xcorr(s1a,s2a);
+r1a=r1a*(1/min(length(s1a), length(s2a)));
+
 r2a=xcorr(s1a,s3a);
+r2a=r2a*(1/min(length(s1a), length(s3a)));
+
+%subplot(4,1,1)
+%stem(r1a)
 
 r1g=xcorr(s1g,s2g);
+r1g=r1g*(1/min(length(s1g), length(s2g)));
 r2g=xcorr(s1g,s3g);
+r2g=r2g*(1/min(length(s1g), length(s3g)));
+
+%subplot(4,1,2)
+%stem(r1g)
 
 r1c=xcorr(s1c,s2c);
+r1c=r1c*(1/min(length(s1c), length(s2c)));
 r2c=xcorr(s1c,s3c);
+r2c=r2c*(1/min(length(s1c), length(s3c)));
+
+%subplot(4,1,3)
+%stem(r1c)
 
 r1t=xcorr(s1t,s2t);
+r1t=r1t*(1/min(length(s1t), length(s2t)));
 r2t=xcorr(s1t,s3t);
+r2t=r2t*(1/min(length(s1t), length(s3t)));
+
+%subplot(4,1,4)
+%stem(r1t)
 
 %% Sum Correlations and Plot
 seq1corr=r1a+r1t+r1g+r1c;
 seq2corr=r2a+r2t+r2g+r2c;
 
-x=0:length(r1a)-1;
-subplot(2,1,1); 
-plot(x,seq1corr)
-title('Genome Sequence 1');
-axis([70000 146651 100 400])
+subplot(2,1,1);
+stem(seq1corr)
+title('Correlation of Human Hemoglobin vs. SeqA.txt');
 subplot(2,1,2);
-plot(x,seq2corr)
-title('Genome Sequence 2');
-axis([70000 146651 100 400])
+stem(seq2corr)
+title('Correlation of Human Hemoglobin vs. SeqB.txt');
+hold on
